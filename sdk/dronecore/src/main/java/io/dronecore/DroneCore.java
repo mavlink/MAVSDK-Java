029f09148f6f2ae48ce84ee6fdd7a2b6059ef829
Jonas Vautherin <jonas.vautherin@gmail.com>
2017-12-20 17:52:58 +0100

    add dronecore-action component, sharing a root build.gradle

--- sdk/dronecore/src/main/java/io/dronecore/DroneCore.java
+++ sdk/dronecore/src/main/java/io/dronecore/DroneCore.java
@@ -2,22 +2,29 @@ package io.dronecore;
 
 import io.dronecore.rpc.DroneCoreRPCGrpc;
 import io.dronecore.rpc.DroneCoreRPCGrpc.DroneCoreRPCBlockingStub;
+import io.grpc.ConnectivityState;
 import io.grpc.ManagedChannel;
 import io.grpc.okhttp.OkHttpChannelBuilder;
 
 public class DroneCore {
-    private ManagedChannel channel;
-    private DroneCoreRPCBlockingStub blockingStub;
+  private ManagedChannel channel;
+  private DroneCoreRPCBlockingStub blockingStub;
 
-    public void connect() {
-        connect("localhost", 50051);
-    }
+  public void connect() {
+    connect("localhost", 50051);
+  }
 
-    public void connect(String host, int port) {
-        channel = OkHttpChannelBuilder.forAddress(host, port)
-                .usePlaintext(true)
-                .build();
+  /**
+   * Connect to the DroneCore backend.
+   *
+   * @param host The address on which the backend is listening.
+   * @param port The port on which the backend is listening.
+   */
+  public void connect(String host, int port) {
+    channel = OkHttpChannelBuilder.forAddress(host, port)
+            .usePlaintext(true)
+            .build();
 
-        blockingStub = DroneCoreRPCGrpc.newBlockingStub(channel);
-   }
+    blockingStub = DroneCoreRPCGrpc.newBlockingStub(channel);
+  }
 }
