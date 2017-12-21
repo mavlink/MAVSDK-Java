9b1835466c22ad75ca80c9c55094f5393a554fd8
Jonas Vautherin <jonas.vautherin@gmail.com>
2017-12-21 17:35:12 +0100

    android-client: add slf4j

--- examples/android-client/app/src/main/java/io/dronecore/dronecoreclient/MainActivity.java
+++ examples/android-client/app/src/main/java/io/dronecore/dronecoreclient/MainActivity.java
@@ -3,11 +3,27 @@ package io.dronecore.dronecoreclient;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.dronecore.DroneCore;
+
 public class MainActivity extends AppCompatActivity {
+
+  private static final Logger logger = LoggerFactory.getLogger(MainActivity.class);
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    logger.trace("Verbose message");
+    logger.debug("Debug message");
+    logger.info("Info message");
+    logger.warn("Warning message");
+    logger.error("Error message");
+
+    DroneCore dc = new DroneCore();
+    dc.connect();
   }
 }
