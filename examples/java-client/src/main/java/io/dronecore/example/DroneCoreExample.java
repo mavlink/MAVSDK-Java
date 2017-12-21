daeb20304adfb17a13cb6f95e36c77d468a42ae1
Jonas Vautherin <jonas.vautherin@gmail.com>
2017-12-21 14:26:17 +0100

    java-client: enable checkstyle (javadoc is made optional there)

--- examples/java-client/src/main/java/io/dronecore/example/DroneCoreExample.java
+++ examples/java-client/src/main/java/io/dronecore/example/DroneCoreExample.java
@@ -3,16 +3,16 @@ package io.dronecore.example;
 import io.dronecore.DroneCore;
 
 public class DroneCoreExample {
-    public static void main(String[] args) {
-        System.out.println("starting...");
+  public static void main(String[] args) {
+    System.out.println("starting...");
 
-        DroneCore dc = new DroneCore();
-        dc.connect();
+    DroneCore dc = new DroneCore();
+    dc.connect();
 
-        try {
-            Thread.sleep(10000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+    try {
+      Thread.sleep(10000);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
     }
+  }
 }
