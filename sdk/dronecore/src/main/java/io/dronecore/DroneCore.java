1be3a166aeafbf9cb6a641f708ca423b1841733b
Jonas Vautherin <jonas.vautherin@gmail.com>
2017-12-21 17:59:40 +0100

    sdk: add slf4j

--- sdk/dronecore/src/main/java/io/dronecore/DroneCore.java
+++ sdk/dronecore/src/main/java/io/dronecore/DroneCore.java
@@ -2,11 +2,15 @@ package io.dronecore;
 
 import io.dronecore.rpc.DroneCoreRPCGrpc;
 import io.dronecore.rpc.DroneCoreRPCGrpc.DroneCoreRPCBlockingStub;
-import io.grpc.ConnectivityState;
 import io.grpc.ManagedChannel;
 import io.grpc.okhttp.OkHttpChannelBuilder;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class DroneCore {
+  private static final Logger logger = LoggerFactory.getLogger(DroneCore.class);
+
   private ManagedChannel channel;
   private DroneCoreRPCBlockingStub blockingStub;
 
@@ -21,6 +25,8 @@ public class DroneCore {
    * @param port The port on which the backend is listening.
    */
   public void connect(String host, int port) {
+    logger.debug("Connecting to " + host + ":" + port);
+
     channel = OkHttpChannelBuilder.forAddress(host, port)
             .usePlaintext(true)
             .build();
