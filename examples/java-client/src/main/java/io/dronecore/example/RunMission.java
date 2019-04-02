12d7470c01e1ab6f08371b73783a3539eb752b1e
Jonas Vautherin <jonas.vautherin@gmail.com>
2019-04-03 01:39:46 +0200

    java-client: add mission example

---
+++ examples/java-client/src/main/java/io/dronecore/example/RunMission.java
@@ -0,0 +1,63 @@
+package io.dronecore.example;
+
+import io.dronecode_sdk.action.Action;
+import io.dronecode_sdk.mission.Mission;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class RunMission {
+  private static final Logger logger = LoggerFactory.getLogger(RunMission.class);
+
+  public static void main(String[] args) {
+    logger.debug("Starting example: mission...");
+
+    List<Mission.MissionItem> missionItems = new ArrayList<>();
+    missionItems.add(generateMissionItem(47.398039859999997, 8.5455725400000002));
+    missionItems.add(generateMissionItem(47.398036222362471, 8.5450146439425509));
+    missionItems.add(generateMissionItem(47.397825620791885, 8.5450092830163271));
+    missionItems.add(generateMissionItem(47.397832880000003, 8.5455939999999995));
+
+    Action action = new Action();
+    Mission mission = new Mission();
+    mission
+        .setReturnToLaunchAfterMission(true)
+        .andThen(mission.uploadMission(missionItems)
+            .doOnComplete(() -> logger.debug("Upload succeeded")))
+        .andThen(action.arm())
+        .andThen(mission.startMission()
+            .doOnComplete(() -> logger.debug("Mission started")))
+        .subscribe();
+
+    CountDownLatch latch = new CountDownLatch(1);
+    mission
+        .getMissionProgress()
+        .filter(progress -> progress.getCurrentItemIndex() == progress.getMissionCount())
+        .take(1)
+        .subscribe(ignored -> latch.countDown());
+
+    try {
+      latch.await();
+    } catch (InterruptedException ignored) {
+      // This is expected
+    }
+  }
+
+  public static Mission.MissionItem generateMissionItem(double latitudeDeg, double longitudeDeg) {
+    return new Mission.MissionItem(
+        latitudeDeg,
+        longitudeDeg,
+        10f,
+        10f,
+        true,
+        Float.NaN,
+        Float.NaN,
+        Mission.MissionItem.CameraAction.NONE,
+        Float.NaN,
+        1.0);
+  }
+}
+
--- examples/java-client/src/main/java/io/dronecore/example/SetRtlAltitude.java
+++ examples/java-client/src/main/java/io/dronecore/example/SetRtlAltitude.java
@@ -5,7 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class SetRtlAltitude {
-  private static final Logger logger = LoggerFactory.getLogger(TakeoffAndLand.class);
+  private static final Logger logger = LoggerFactory.getLogger(SetRtlAltitude.class);
 
   public static void main(String[] args) {
     logger.debug("Starting example: set RTL altitude...");
