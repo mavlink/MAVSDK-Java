6c1678b42fb3c6668344cc6da2174df147ccd10c
Jonas Vautherin <jonas.vautherin@gmail.com>
2019-04-03 00:46:18 +0200

    java-client: reorganize examples

--- examples/java-client/src/main/java/io/dronecore/example/TakeoffAndLand.java
+++ examples/java-client/src/main/java/io/dronecore/example/TakeoffAndLand.java
@@ -1,26 +1,19 @@
 package io.dronecore.example;
 
 import io.dronecode_sdk.action.Action;
-import io.reactivex.schedulers.Schedulers;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class DroneCoreExample {
-  public static final Logger logger = LoggerFactory.getLogger(DroneCoreExample.class);
+public class TakeoffAndLand {
+  private static final Logger logger = LoggerFactory.getLogger(TakeoffAndLand.class);
 
   public static void main(String[] args) {
-    logger.debug("Starting example...");
-
-    CountDownLatch latch = new CountDownLatch(1);
+    logger.debug("Starting example: takeoff and land...");
 
     Action action = new Action();
-
-    action.setReturnToLaunchAltitude(15f).blockingAwait();
-    logger.debug("RTL altitude: " + action.getReturnToLaunchAltitude().blockingGet());
-    action.setReturnToLaunchAltitude(20f).blockingAwait();
-    logger.debug("RTL altitude: " + action.getReturnToLaunchAltitude().blockingGet());
+    CountDownLatch latch = new CountDownLatch(1);
 
     action.arm()
           .andThen(action.takeoff())
