7c2153309b9a9a651d17aab35f3619da5e026b10
Ryan Hurst <ryanhurst1@hotmail.com>
2019-04-10 15:59:35 -0600

    typos

--- examples/android-client/app/src/androidTest/java/io/dronecore/dronecoreclient/ExampleInstrumentedTest.java
+++ examples/android-client/app/src/androidTest/java/io/dronecore/dronecoreclient/ExampleInstrumentedTest.java
@@ -22,6 +22,6 @@ public class ExampleInstrumentedTest {
     // Context of the app under test.
     Context appContext = InstrumentationRegistry.getTargetContext();
 
-    assertEquals("com.tealdrones.map", appContext.getPackageName());
+    assertEquals("io.dronecore.dronecoreclient", appContext.getPackageName());
   }
 }
