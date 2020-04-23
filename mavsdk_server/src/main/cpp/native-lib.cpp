258246a78ecf5c4048df685fa238bc1804e4d3a3
Jonas Vautherin <jonas.vautherin@gmail.com>
2020-04-23 15:16:14 +0200

    Move to new mavsdk_server API (that can be started and stopped)

--- mavsdk_server/src/main/cpp/native-lib.cpp
+++ mavsdk_server/src/main/cpp/native-lib.cpp
@@ -1,18 +1,56 @@
 #include <android/log.h>
-#include <jni.h>
 #include <backend_api.h>
+#include <future>
+#include <jni.h>
+#include <thread>
 
 #define  LOGD(...)  __android_log_print(ANDROID_LOG_DEBUG,"MAVSDK-Server",__VA_ARGS__)
 
 extern "C"
 {
-    JNIEXPORT void JNICALL
-    Java_io_mavsdk_mavsdkserver_MavsdkServer_run(JNIEnv* env, jobject thiz, jstring system_address, jint mavsdk_server_port)
+    void onServerStarted(void* port_promise) {
+        auto* promise = static_cast<std::promise<void>*>(port_promise);
+        promise->set_value();
+    }
+
+    JNIEXPORT jlong JNICALL
+    Java_io_mavsdk_mavsdkserver_MavsdkServer_runNative(JNIEnv* env, jobject thiz, jstring system_address, jint mavsdk_server_port)
     {
         const char* native_connection_url = env->GetStringUTFChars(system_address, 0);
 
         LOGD("Running mavsdk_server with connection url: %s", native_connection_url);
-        runBackend(native_connection_url, mavsdk_server_port, nullptr, nullptr);
-        //runBackend("serial:///dev/bus/usb/001/002", nullptr, nullptr);
+        std::promise<int> port_promise;
+        auto port_future = port_promise.get_future();
+
+        auto mavsdk_server = runBackend(
+                native_connection_url,
+                mavsdk_server_port,
+                onServerStarted,
+                &port_promise);
+        auto server_port = port_future.get();
+        LOGD("mavsdk_server is now running, listening on port %d", server_port);
+
+        return reinterpret_cast<jlong>(mavsdk_server);
+    }
+
+    JNIEXPORT jint JNICALL
+    Java_io_mavsdk_mavsdkserver_MavsdkServer_getPort(JNIEnv* env, jobject thiz, jlong mavsdkServerHandle)
+    {
+        auto mavsdk_server = reinterpret_cast<MavsdkBackend*>(mavsdkServerHandle);
+        return getPort(mavsdk_server);
+    }
+
+    JNIEXPORT void JNICALL
+    Java_io_mavsdk_mavsdkserver_MavsdkServer_attach(JNIEnv* env, jobject thiz, jlong mavsdkServerHandle)
+    {
+        auto mavsdk_server = reinterpret_cast<MavsdkBackend*>(mavsdkServerHandle);
+        attach(mavsdk_server);
+    }
+
+    JNIEXPORT void JNICALL
+    Java_io_mavsdk_mavsdkserver_MavsdkServer_stop(JNIEnv* env, jobject thiz, jlong mavsdkServerHandle)
+    {
+        auto mavsdk_server = reinterpret_cast<MavsdkBackend*>(mavsdkServerHandle);
+        stopBackend(mavsdk_server);
     }
 };
--- mavsdk_server/src/main/java/io/mavsdk/mavsdkserver/MavsdkServer.java
+++ mavsdk_server/src/main/java/io/mavsdk/mavsdkserver/MavsdkServer.java
@@ -2,9 +2,77 @@ package io.mavsdk.mavsdkserver;
 
 public class MavsdkServer {
 
-    static {
-        java.lang.System.loadLibrary("native_lib");
-    }
+  static {
+    java.lang.System.loadLibrary("native_lib");
+  }
 
-    public native void run(String systemAddress, int mavsdkServerPort);
+  private long mavsdkServerHandle;
+
+  /**
+   * Run MavsdkServer with MAVLink defaulting to udp://:14540.
+   *
+   * <p>MavsdkServer will start a gRPC server listening on an
+   * arbitrary port, to which a `System` should connect.</p>
+   *
+   * @return The port on which MavsdkServer listens for a `System` to connect.
+   *     A return value of 0 means that the server failed to start.
+   */
+  public int run() {
+    return run("udp://:14540");
+  }
+
+  /**
+   * Run MavsdkServer with MAVLink on `systemAddress`.
+   *
+   * <p>MavsdkServer will start a gRPC server listening on an
+   * arbitrary port, to which a `System` should connect.</p>
+   *
+   * @param systemAddress The address on which the remote MAVLink system is expected.
+   *     Valid formats are:
+   *     For TCP : tcp://[server_host][:server_port].
+   *     For UDP : udp://[bind_host][:bind_port].
+   *     For Serial : serial:///path/to/serial/dev[:baudrate].
+   * @return The port on which MavsdkServer listens for a `System` to connect.
+   *     A return value of 0 means that the server failed to start.
+   */
+  public int run(String systemAddress) {
+    return run(systemAddress, 0);
+  }
+
+  /**
+   * Run MavsdkServer with MAVLink on `systemAddress`.
+   *
+   * <p>MavsdkServer will listen for a `System` to connect on `mavsdkServerPort`.</p>
+   *
+   * @param systemAddress The address on which the remote MAVLink system is expected.
+   *     Valid formats are:
+   *     For TCP : tcp://[server_host][:server_port]
+   *     For UDP : udp://[bind_host][:bind_port]
+   *     For Serial : serial:///path/to/serial/dev[:baudrate]
+   * @param mavsdkServerPort The port on which the server should listen for a `System`.
+   * @return A handle to the started MavsdkServer, to be used with other native functions.
+   */
+  public int run(String systemAddress, int mavsdkServerPort) {
+    mavsdkServerHandle = runNative(systemAddress, mavsdkServerPort);
+    return getPort(mavsdkServerHandle);
+  }
+
+  private native long runNative(String systemAddress, int mavsdkServerPort);
+
+  private native int getPort(long mavsdkServerHandle);
+
+  /**
+   * Attach to the running MavsdkServer, effectively blocking until it stops.
+   */
+  public void attach() {
+    attach(mavsdkServerHandle);
+  }
+
+  private native void attach(long mavsdkServerHandle);
+
+  public void stop() {
+    stop(mavsdkServerHandle);
+  }
+
+  private native void stop(long mavsdkServerHandle);
 }
