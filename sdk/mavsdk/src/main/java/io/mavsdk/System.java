2ece1c9719418090e74e33ff4db64ce5bcf8e437
Jonas Vautherin <jonas.vautherin@gmail.com>
2021-01-05 15:35:07 +0100

    Update proto submodule and add remaining plugins to System helper

--- sdk/mavsdk/src/main/java/io/mavsdk/System.java
+++ sdk/mavsdk/src/main/java/io/mavsdk/System.java
@@ -4,24 +4,44 @@ import io.mavsdk.action.Action;
 import io.mavsdk.calibration.Calibration;
 import io.mavsdk.camera.Camera;
 import io.mavsdk.core.Core;
+import io.mavsdk.failure.Failure;
+import io.mavsdk.follow_me.FollowMe;
+import io.mavsdk.ftp.Ftp;
+import io.mavsdk.geofence.Geofence;
 import io.mavsdk.gimbal.Gimbal;
 import io.mavsdk.info.Info;
+import io.mavsdk.log_files.LogFiles;
+import io.mavsdk.manual_control.ManualControl;
 import io.mavsdk.mission.Mission;
+import io.mavsdk.mission_raw.MissionRaw;
+import io.mavsdk.mocap.Mocap;
 import io.mavsdk.offboard.Offboard;
 import io.mavsdk.param.Param;
+import io.mavsdk.shell.Shell;
 import io.mavsdk.telemetry.Telemetry;
+import io.mavsdk.tune.Tune;
 
 public class System {
   private Action action;
   private Calibration calibration;
   private Camera camera;
   private Core core;
+  private Failure failure;
+  private FollowMe followMe;
+  private Ftp ftp;
+  private Geofence geofence;
   private Gimbal gimbal;
   private Info info;
+  private LogFiles logFiles;
+  private ManualControl manualControl;
   private Mission mission;
+  private MissionRaw missionRaw;
+  private Mocap mocap;
   private Offboard offboard;
   private Param param;
+  private Shell shell;
   private Telemetry telemetry;
+  private Tune tune;
 
   /**
    * Create a System object, initializing the plugins and connecting them to mavsdk_server.
@@ -42,12 +62,22 @@ public class System {
     this.calibration = new Calibration(host, port);
     this.camera = new Camera(host, port);
     this.core = new Core(host, port);
+    this.failure = new Failure(host, port);
+    this.followMe = new FollowMe(host, port);
+    this.ftp = new Ftp(host, port);
+    this.geofence = new Geofence(host, port);
     this.gimbal = new Gimbal(host, port);
     this.info = new Info(host, port);
+    this.logFiles = new LogFiles(host, port);
+    this.manualControl = new ManualControl(host, port);
     this.mission = new Mission(host, port);
+    this.missionRaw = new MissionRaw(host, port);
+    this.mocap = new Mocap(host, port);
     this.offboard = new Offboard(host, port);
     this.param = new Param(host, port);
+    this.shell = new Shell(host, port);
     this.telemetry = new Telemetry(host, port);
+    this.tune = new Tune(host, port);
   }
 
   public Action getAction() {
@@ -66,6 +96,22 @@ public class System {
     return core;
   }
 
+  public Failure getFailure() {
+    return failure;
+  }
+
+  public FollowMe getFollowMe() {
+    return followMe;
+  }
+
+  public Ftp getFtp() {
+    return ftp;
+  }
+
+  public Geofence getGeofence() {
+    return geofence;
+  }
+
   public Gimbal getGimbal() {
     return gimbal;
   }
@@ -74,8 +120,24 @@ public class System {
     return info;
   }
 
+  public LogFiles getLogFiles() {
+    return logFiles;
+  }
+
+  public ManualControl getManualControl() {
+    return manualControl;
+  }
+
   public Mission getMission() {
     return mission;
+  }
+
+  public MissionRaw getMissionRaw() {
+    return missionRaw;
+  }
+
+  public Mocap getMocap() {
+    return mocap;
   }
 
   public Offboard getOffboard() {
@@ -86,8 +148,16 @@ public class System {
     return param;
   }
 
+  public Shell getShell() {
+    return shell;
+  }
+
   public Telemetry getTelemetry() {
     return telemetry;
+  }
+
+  public Tune getTune() {
+    return tune;
   }
 
   /**
@@ -98,11 +168,21 @@ public class System {
     this.calibration.dispose();
     this.camera.dispose();
     this.core.dispose();
+    this.failure.dispose();
+    this.followMe.dispose();
+    this.ftp.dispose();
+    this.geofence.dispose();
     this.gimbal.dispose();
     this.info.dispose();
+    this.logFiles.dispose();
+    this.manualControl.dispose();
     this.mission.dispose();
+    this.missionRaw.dispose();
+    this.mocap.dispose();
     this.offboard.dispose();
     this.param.dispose();
+    this.shell.dispose();
     this.telemetry.dispose();
+    this.tune.dispose();
   }
 }
