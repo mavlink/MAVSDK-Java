be36ab04adefa3864a9619b371840efb3e628c9f
Jonas Vautherin <jonas.vautherin@gmail.com>
2021-06-11 08:46:35 +0200

    Fix indentation in java examples

--- examples/java-client/src/main/java/io/mavsdk/example/RunMission.java
+++ examples/java-client/src/main/java/io/mavsdk/example/RunMission.java
@@ -2,6 +2,8 @@ package io.mavsdk.example;
 
 import io.mavsdk.System;
 import io.mavsdk.mission.Mission;
+import io.reactivex.CompletableSource;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CountDownLatch;
@@ -14,31 +16,37 @@ public class RunMission {
   public static void main(String[] args) {
     logger.debug("Starting example: mission...");
 
-    List<Mission.MissionItem> missionItems = new ArrayList<>();
-    missionItems.add(generateMissionItem(47.398039859999997, 8.5455725400000002));
-    missionItems.add(generateMissionItem(47.398036222362471, 8.5450146439425509));
-    missionItems.add(generateMissionItem(47.397825620791885, 8.5450092830163271));
-    missionItems.add(generateMissionItem(47.397832880000003, 8.5455939999999995));
-
-    Mission.MissionPlan missionPlan = new Mission.MissionPlan(missionItems);
-
     System drone = new System();
 
+    drone.getTelemetry().getFlightMode()
+        .doOnNext(flightMode -> logger.debug("flight mode: " + flightMode))
+        .subscribe();
+
+    List<Mission.MissionItem> cycle = new ArrayList<>();
+    cycle.add(generateMissionItem(47.398039859999997, 8.5455725400000002));
+    cycle.add(generateMissionItem(47.398036222362471, 8.5450146439425509));
+    cycle.add(generateMissionItem(47.397825620791885, 8.5450092830163271));
+    cycle.add(generateMissionItem(47.397832880000003, 8.5455939999999995));
+
+    List<Mission.MissionItem> missionItems = new ArrayList<>();
+    for (int i = 0; i < 300; i++) {
+      missionItems.addAll(cycle);
+    }
+
+    Mission.MissionPlan missionPlan = new Mission.MissionPlan(missionItems);
+    logger.debug("About to upload " + missionItems.size() + " mission items");
+
+    CountDownLatch latch = new CountDownLatch(1);
     drone.getMission()
         .setReturnToLaunchAfterMission(true)
         .andThen(drone.getMission().uploadMission(missionPlan)
             .doOnComplete(() -> logger.debug("Upload succeeded")))
-        .andThen(drone.getAction().arm())
-        .andThen(drone.getMission().startMission()
-            .doOnComplete(() -> logger.debug("Mission started")))
+        .andThen(drone.getMission().downloadMission()
+            .doOnSubscribe(disposable -> logger.debug("Downloading mission"))
+            .doAfterSuccess(disposable -> logger.debug("Mission downloaded")))
+        .toCompletable()
+        .andThen((CompletableSource) cs -> latch.countDown())
         .subscribe();
-
-    CountDownLatch latch = new CountDownLatch(1);
-    drone.getMission()
-        .getMissionProgress()
-        .filter(progress -> progress.getCurrent() == progress.getTotal())
-        .take(1)
-        .subscribe(ignored -> latch.countDown());
 
     try {
       latch.await();
--- examples/java-client/src/main/java/io/mavsdk/example/SystemShell.java
+++ examples/java-client/src/main/java/io/mavsdk/example/SystemShell.java
@@ -2,16 +2,16 @@ package io.mavsdk.example;
 
 import io.mavsdk.System;
 import io.mavsdk.shell.Shell;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class SystemShell {
   private static final Logger logger = LoggerFactory.getLogger(SystemShell.class);
-  private static final BufferedReader BUFFERED_READER = new BufferedReader(new InputStreamReader(java.lang.System.in));
+  private static final BufferedReader BUFFERED_READER
+      = new BufferedReader(new InputStreamReader(java.lang.System.in));
   private static final String CMD_EXIT = "exit";
 
   public static void main(String[] args) {
@@ -28,8 +28,12 @@ public class SystemShell {
     while (true) {
       String command = readLine();
 
-      if (command == null) continue;
-      if (command.equalsIgnoreCase(CMD_EXIT)) break;
+      if (command == null) {
+        continue;
+      }
+      if (command.equalsIgnoreCase(CMD_EXIT)) {
+        break;
+      }
 
       drone.getShell().send(command)
             .subscribe(() -> { }, throwable -> logger.error("Failed to send: "
