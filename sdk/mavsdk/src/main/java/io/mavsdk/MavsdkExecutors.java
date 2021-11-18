2d1bfeccb855d438bfb4427b3f3601bcb2d53b08
divyanshu <divyanshu.pundir@gmail.com>
2021-11-18 13:52:07 +0530

    feat: Created MavsdkExecutors for handling connection threading needs

---
+++ sdk/mavsdk/src/main/java/io/mavsdk/MavsdkExecutors.java
@@ -0,0 +1,21 @@
+package io.mavsdk;
+
+import io.reactivex.annotations.NonNull;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+public class MavsdkExecutors {
+
+  private MavsdkExecutors() {
+  }
+
+  private static final class Holder {
+    private static final Executor CONNECTION_EXECUTOR = Executors.newSingleThreadExecutor();
+  }
+
+  @NonNull
+  public static Executor connectionExecutor() {
+    return Holder.CONNECTION_EXECUTOR;
+  }
+}
