2e661a914cda660b563a6b58fc1aeb3d9b0cd28a
rayw-dronesense <131709565+rayw-dronesense@users.noreply.github.com>
2024-11-26 10:11:03 -0500

    Update mavsdk_server/src/main/java/io/mavsdk/mavsdkserver/MavsdkServer.java
    Co-authored-by: Jonas Vautherin <accounts@jonas.vautherin.ch>

--- mavsdk_server/src/main/java/io/mavsdk/mavsdkserver/MavsdkServer.java
+++ mavsdk_server/src/main/java/io/mavsdk/mavsdkserver/MavsdkServer.java
@@ -74,7 +74,7 @@ public class MavsdkServer {
    *     For UDP : udp://[bind_host][:bind_port]
    *     For Serial : serial:///path/to/serial/dev[:baudrate]
    * @param mavsdkServerPort The port on which the server should listen for a `System`.
-   * @param systemId The identifier which the system should connect with.
+   * @param systemId The MAVLink sysid that MAVSDK should use.
    * @param componentId The identifier which the component should connect with.
    * @return The port on which MavsdkServer listens for a `System` to connect.
    *     A return value of 0 means that the server failed to start.
